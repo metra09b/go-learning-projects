f88f5b7269ea2ba525cf106eafa2f2e0cd816520
metra09b <artem53bystrov2@gmail.com>
2026-06-24 21:07:03 +0300

    1 + 2 complete

---
+++ yandex-course/level2-step3-errors/2)reliableSeparation.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"errors"
+)
+
+var DivisionByZeroError = errors.New("division by zero is not allowed")
+
+func Divide(a, b int) (float64, error) {
+	if b == 0 {
+		return 0, DivisionByZeroError
+	} else {
+		return float64(a) / float64(b), nil
+	}
+}
